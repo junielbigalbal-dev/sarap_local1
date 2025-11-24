6ae7d6c395b98b067401f23f6f2a615da01df883
Sarap Local Admin <admin@saraplocal.com>
2025-11-24 16:14:27 +0800

    Fix: Undefined array key QUERY_STRING warning in products.php

--- pages/customer/products.php
+++ pages/customer/products.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../../config/session.php';
 require_once __DIR__ . '/../../includes/functions.php';
 
 // Redirect to the persistent dashboard view
-$queryString = $_SERVER['QUERY_STRING'] ? '?' . $_SERVER['QUERY_STRING'] : '';
-redirect(SITE_URL . '/pages/customer/dashboard.php' . ($queryString ? $queryString . '&view=products' : '?view=products'));
+$queryString = $_SERVER['QUERY_STRING'] ?? '';
+redirect(SITE_URL . '/pages/customer/dashboard.php' . ($queryString ? '?' . $queryString . '&view=products' : '?view=products'));
 exit;
 ?>
